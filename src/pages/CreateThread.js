0c94325d3aa1b170a6c16acd7d0f791fbf6081eb
Tinnnooo <113224141+Tinnnooo@users.noreply.github.com>
2023-01-05 13:10:43 +0700

    initial commit

--- src/pages/CreateThread.js
+++ src/pages/CreateThread.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { asyncAddThread } from '../states/threads/action';
 import ThreadInput from '../components/ThreadInput';
+import { asyncPopulateUsersAndThreads } from '../states/shared/action';
 
 function CreateThread() {
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncPopulateUsersAndThreads());
+  }, [dispatch]);
 
   const onThreadInput = (title, body, category) => {
     dispatch(asyncAddThread({ title, body, category }));
